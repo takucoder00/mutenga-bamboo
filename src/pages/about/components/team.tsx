354f780742ee9d1fe96f8122c18fc9003c629156
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-11-07 10:52:28 +0200

    🤽🏽‍♂️ Added new team member

--- src/pages/about/components/team.tsx
+++ src/pages/about/components/team.tsx
@@ -21,6 +21,15 @@ export default function OurTeam() {
       </div>
       <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={process.env.PUBLIC_URL + '/images/team/5.jpeg'}/>
+          <div className="flex-grow">
+            <h2 className="text-gray-900 title-font font-medium">Farai Musendo</h2>
+            <p className="text-gray-500">Co Founder</p>
+          </div>
+        </div>
+      </div>
+      <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
           <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={process.env.PUBLIC_URL + '/images/team/4.jpeg'}/>
           <div className="flex-grow">
             <h2 className="text-gray-900 title-font font-medium">Lianda Meda</h2>
