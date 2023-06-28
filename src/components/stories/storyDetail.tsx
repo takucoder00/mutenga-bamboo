596c455f935b3d743c5c51fb0b20219f17957866
Takunda Madechangu <takundam@sonity.net>
2023-06-28 10:32:35 +0200

    Changed Image height and with

--- src/components/stories/storyDetail.tsx
+++ src/components/stories/storyDetail.tsx
@@ -1,5 +1,8 @@
 import Base from "@/pages/base"
+import { Image, Title } from "@mantine/core"
 import React from "react"
+import { StoryPostBy } from "./storyPostBy"
+
 
 
 
@@ -9,6 +12,19 @@ export function StoryDetail(){
 
     return (
      <Base>
+
+     <Image 
+       src={"https://images.unsplash.com/photo-1687789256307-839c77ed376d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1033&q=80"} 
+       height={420} width={1000}
+       />
+
+       <StoryPostBy date={"16 Dec 2021"}/>
+
+       <Title order={1}>President Emmerson Mnangawnan did so and so</Title>
+
+       
+
+
 
      
      </Base>
