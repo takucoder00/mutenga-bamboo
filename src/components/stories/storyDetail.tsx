9c4244ff6d2ff1eda726c04ba768e1bbe8532b70
Takunda Madechangu <takundam@sonity.net>
2023-06-30 09:12:26 +0200

    Style story detail a little bit

--- src/components/stories/storyDetail.tsx
+++ src/components/stories/storyDetail.tsx
@@ -10,18 +10,29 @@ export function StoryDetail({post}: { post: BlogPost}){
     return (
      <Base>
 
+     <div className="max-w-2xl px-6 pt-6 pb-24 mx-auto space-y-12">
+
      <Image 
        src={`${process.env.STORAGE_PATH}/${post.image}`} 
-       height={420} width={1000}
+       height={420}
        />
 
-       <StoryPostBy date={"16 Dec 2021"}/>
+<p className="text-sm text-gray-400"> by <a rel="noopener noreferrer" href="#" target="_blank" className="underline text-green-400">
+      <span itemProp="name">Mutenga Bamboo</span></a> on <time dateTime="2021-02-12 15:34:18-0200">Feb 12th 2021</time></p>
+
+       
        <Divider my="sm" />
        <Title order={1}>{ post.title }</Title>
 
-       <article className="prose md:prose-lg lg:prose-xl">
+       <article className="">
         {post.content}
        </article>
+
+         <StoryPostBy date={"16 Dec 2021"}/>
+
+       </div>
+
+     
      </Base>
     )
 }
