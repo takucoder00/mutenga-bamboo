eb5950e518e3e44f0db71e176b70afc59a9cbd94
Takunda Madechangu <takundam@sonity.net>
2023-07-01 22:46:40 +0200

    Refactor

--- src/components/about/aboutMutengaBamboo.tsx
+++ src/components/about/aboutMutengaBamboo.tsx
@@ -1,5 +1,6 @@
 import { Divider, Text, Title } from '@mantine/core'
 import React from 'react'
+import CustomPageTitle from '../general/CustomPageTitle'
 
 
 export default function AboutMutengaBamboo() {
@@ -7,21 +8,9 @@ export default function AboutMutengaBamboo() {
         <div className="bg-gray-50">
             <div className=" px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
   <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-    
-    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-      <span className="relative inline-block">
-        <svg viewBox="0 0 52 24" fill="currentColor" className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
-          <defs>
-            <pattern id="3071a3ad-db4a-4cbe-ac9a-47850b69e4b7" x="0" y="0" width=".135" height=".30">
-              <circle cx="1" cy="1" r=".7"></circle>
-            </pattern>
-          </defs>
-          
-        </svg>
-       
-      </span>
-      About Us
-    </h2>
+  
+
+    <CustomPageTitle title={"About Us"} />
     <p className="text-base text-gray-700 md:text-lg">
     We promote rural industrialization, We are environment conscious and Bamboo can save the planet
     </p>
