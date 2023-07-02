4d54694cb51e9b44369ff2e681226feb56b6097b
Takunda Madechangu <takundam@sonity.net>
2023-07-02 15:22:14 +0200

    Update urls

--- src/global.ts
+++ src/global.ts
@@ -5,9 +5,9 @@ import axios from "axios";
 let mode = process.env.NODE_ENV
 
 
-export const apiEndpoint = mode == "development" ? 'http://localhost:8000/api' : 'https://admin.mutengabamboo.co.zw/api/';
+export const apiEndpoint = mode == "development" ? 'http://localhost:8000/api' : 'https://dash.mutengabamboo.co.zw/api/';
 export const siteEndpoint = mode == "development" ? 'http://localhost:3000/' : 'https://admin.mutengabamboo.co.zw/';
-export const storageEndpoint = mode == "development" ? 'http://localhost:8000/storage' : "https://mutengabamboo.co.zw/storage";
+export const storageEndpoint = mode == "development" ? 'http://localhost:8000/storage' : "https://dash.mutengabamboo.co.zw/storage";
 
 
 
