37da41c2b4714bc094d310bb0d4a96968e0539d5
Takunda Madechangu <takundam@sonity.net>
2023-07-05 11:36:28 +0200

    Change image height and width

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['localhost', 'mutengabamboo.co.zw']
+  },
   compiler: {
     styledComponents: {
       displayName: true,
